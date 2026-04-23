39230cc796c7f2bee864860beac629ff055e661e
S0ra-0 <79579919+S0ra-0-0@users.noreply.github.com>
2026-04-23 11:41:33 +0200

    FEAT: Player select,

--- Assets/Devs/Scripts/CharachterSelect.cs
+++ Assets/Devs/Scripts/CharachterSelect.cs
@@ -6,11 +6,14 @@ public class CharachterSelect : MonoBehaviour
     [SerializeField] private GameObject[] PlayerJoinSquares;
     [SerializeField] private PlayerInputManager playerInputManager;
     private int playerCount = 0;
+    private readonly Color[] playerColors = new Color[] { Color.red, Color.blue, Color.green, Color.yellow };
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
         GameObject player = playerInput.gameObject;
         player.transform.position = new Vector2(PlayerJoinSquares[playerCount].transform.position.x, (PlayerJoinSquares[playerCount].transform.position.y) + 5f);
+        player.GetComponent<Renderer>().material.color = playerColors[playerCount];
+        player.GetComponent<Player>().SpawnCursor(playerColors[playerCount]);
         playerCount++;
     }
 
